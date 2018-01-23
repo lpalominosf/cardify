5a8d070ef0eb36704b851369d4e4751cdb810609
gabycasti <castillg@gmail.com>
2018-01-23 12:41:03 -0300

    agregar archivo mocha

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,12 @@
-$(document).ready(function(){
-var buscar = $("#container").find("img");
-if (buscar.length > 0){
-    console.log(buscar);
-} 
+$(document).ready(function() {
+    var buscar = $("#container").find("img");
+    if (buscar.length > 0) {
+        console.log(buscar);
+
+        $('#container').attr('style', 'border: 1px solid red;')
+    }
+
+
+
 
 });
