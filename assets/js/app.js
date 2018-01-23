9e0ffeadd51bf0d37d75d6869779e1852dc0335b
pazautumn <pazcontrerastello@gmail.com>
2018-01-23 12:39:03 -0300

    eslint

--- assets/js/app.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-var buscar = $("#container").find("img");
-if (buscar.length > 0){
-    console.log(buscar);
-} 
-
-});
---
+++ src/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    var buscar = $('#container').find('img');
+    if (buscar.length > 0) {
+        console.log(buscar);
+    };
+});
