99becd7763129ecc211bd919e0879ef4b26ae071
PazAutumn <pazcontrerastello@gmail.com>
2018-01-23 23:32:41 -0300

    aprendiendo test

--- src/app.js
+++ src/app.js
@@ -3,4 +3,10 @@ $(document).ready(function() {
     if (buscar.length > 0) {
         console.log(buscar);
     };
+
+    var archivo = $("#file").val();
+    var extensiones = archivo.substring(archivo.lastIndexOf("."));
+    if (extensiones != ".jpg" || extensiones != ".jpg" || extensiones != ".jpeg") {
+        alert("El archivo de tipo " + extensiones + "no es válido");
+    }
 });
--- test/test.js
+++ test/test.js
@@ -1,4 +1,19 @@
-var assert = chai.assert;
-var chai = require('chai.assert');
+var extension = require('../src/app');
 
+var assertEqual = function(value, expected) {
+  if (value !== expected) {
+    throw new Error(`Expected '${value}' to equal '${expected}'`);
+  }
+};
 
+describe('extension()', function() {
+
+  it('debería devolver true para archivos con extensión .jpg, .jpeg o .png', function() {
+    assertEqual(extension(''), true);
+  });
+
+  it('debería devolver false para archivos con extensión diferente a .jpg, .jpeg o .png', () => {
+    assertEqual(isVowel('b'), false);
+  });
+
+});
