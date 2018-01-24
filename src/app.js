24e8c8bec71d11bf023d636f23623893958a6aa3
gabycasti <castillg@gmail.com>
2018-01-24 18:50:24 -0300

    agregando figure a la imagen

--- src/app.js
+++ src/app.js
@@ -1,34 +1,32 @@
-$(document).ready(function() {
-    var buscar = $('#container').find('img');
-    if (buscar.length > 0) {
-        console.log(buscar);
-    
-    };
+//Llamando el plugin al div container
+$(document).ready(function(){
+   $('#container').nombre_del_plugin();
+  
+  });
 
 (function ($){
   jQuery.fn.nombre_del_plugin = function() {
+ var buscar = $('#container').find('img');
+  if (buscar.length > 0) {
+     console.log(buscar);
+     for(i=0; i<=buscar.length; i++)
+     $(buscar[i]).wrap('<figure></figure>');
+     var padre =  buscar[i].parent();
+     var mensaje = $(buscar[i]).attr('alt')
+     padre.after('<figcaption>' + mensaje + '</figcaption>')
+    };
 
-$('img').wrap('<figure></figure>');
-
-$('img').after()
   };
 })(jQuery);
 
 
-   //$('img').hover(function(){
-   // var atributo = $(this).attr("alt");
-     // $(this).wrap('<figure></figure>');
-      //$(this).append('<figcaption> +'atributo'+<figcaption>');
-
-   //})
    
 
-});
 
 
-    var archivo = $("#file").val();
-    var extensiones = archivo.substring(archivo.lastIndexOf("."));
-    if (extensiones != ".jpg" || extensiones != ".jpg" || extensiones != ".jpeg") {
-        alert("El archivo de tipo " + extensiones + "no es válido");
-    }
-});
+    //var archivo = $("#file").val();
+    //var extensiones = archivo.substring(archivo.lastIndexOf("."));
+    //if (extensiones != ".jpg" || extensiones != ".jpg" || extensiones != ".jpeg") {
+      //  alert("El archivo de tipo " + extensiones + "no es válido");
+    //}
+//});
