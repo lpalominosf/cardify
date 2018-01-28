25ff89d5ad9d2eb4ad3f76bef01b396105ded40f
Lore Palominos <lorena.palominos@gmail.com>
2018-01-28 20:28:42 -0300

    primer test ok

---
+++ test/test.js
@@ -0,0 +1,28 @@
+const chai = require('chai');
+
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+let dom = new JSDOM('!DOCTYPE html><html lang="en">' +
+  '<head><meta charset ="UTF-8"><title>Proyecto Cardify</title>' +
+  '/head><body><div class="container"><h1>Test</h2>' +
+  '<img src="https://www.anipedia.net/imagenes/gatos-800x375.jpg" alt="cuchito">' +
+  '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">' +
+  '</script><script src="node_modules/jquery/dist/jquery.min.js">' +
+  '</script><script src="assets/js/app.js"></script></body></html>');
+const { window } = dom;
+global.document = dom;
+global.window = window;
+global.navigator = {
+  userAgent: 'node.js'
+};
+
+const $ = require('jquery');
+const cardify = require("../assets/js/app.js");
+var expect = require('chai').expect;
+
+describe('Comprobar si alt tiene una descripción', () => {
+  it('alt contiene descripción', () => {
+    var empty = ($('img').attr('alt').length);
+    expect($('img').attr('alt')).to.have.length(empty);
+  });
+});
