a177b06273ccaf56d087216c3a2a50e54af26d21
pazautumn <pazcontrerastello@gmail.com>
2018-01-29 11:36:07 -0300

    agregando babel

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const cardifyimg = require("src/cardify");
 module.exports = cardifyimg;
 
 /*module.exports=  {
-cardifyimg:function();
-cardifydescription:function();
+ *cardifyimg:function();
+ *cardifydescription:function();
 }*/
